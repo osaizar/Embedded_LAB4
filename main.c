744f32523da9b5f441a4f4e26a244a6b79c5e8f5
Julia Doñoro Martín <juldo607@li2-7.ida.liu.se>
2017-04-24 13:26:23 +0200

    hay cosas que van, otras no

--- main.c
+++ main.c
@@ -8,6 +8,10 @@ void llmultiply2(unsigned long long int l1,
                 unsigned long long int l2,
                 unsigned char *result);
 
+unsigned long int high (unsigned long long int num);
+
+unsigned long int low (unsigned long long int num);
+
 struct test_case {
   unsigned long long int a;
   unsigned long long int b;
@@ -28,7 +32,7 @@ struct test_case cases[6] = {
 
   { 0xFFFFFFFFFFFFFFFFULL, 0xFFFFFFFFFFFFFFFFULL,
     0xFFFFFFFFFFFFFFFEULL, 0x0000000000000001ULL },
-  
+
   { 0x00000001FFFFFFFFULL, 0x00000001FFFFFFFFULL,
     0x0000000000000003ULL, 0xFFFFFFFC00000001ULL },
 
@@ -38,26 +42,61 @@ struct test_case cases[6] = {
 
 void PutUnsignedLongLong(unsigned long long int* ulli)
 {
-  unsigned long int* uli = (unsigned long int*)ulli; 
+  unsigned long int* uli = (unsigned long int*)ulli;
   PutUnsigned(uli[1], 16, 8);
-  PutUnsigned(uli[0], 16, 8); 
+  PutUnsigned(uli[0], 16, 8);
 }
 
-void llmultiply2(unsigned long long int l1, unsigned long long int l2, unsigned char *result)
-{
- unsigned long long int res;
- result = l1*l2;
- result = (l1*l2)/(2^8);
- //result = *res;
- PutUnsignedLongLong(&result+8);
- PutUnsignedLongLong(&result);
+unsigned long int low (unsigned long long int num) {
+	return (((1L << 32) -1) & num);
+}
+
+unsigned long int high (unsigned long long int num) {
+	return (num >> 32);
+}
+
+void llmultiply2(unsigned long long int l1, unsigned long long int l2, unsigned char *result) {
+
+    unsigned long int s0, s1, s2, s3;
+    unsigned long int num = low(l1) * low(l2);
+    unsigned long long res, carry;
+
+    s0 = low(num);
+
+    num = high(l1) * low(l2) + high(num);
+    s1 = low(num);
+    s2 = high(num);
+
+    num = s1 + low(l1) * high(l2);
+    s1 = low(num);
+
+    num = s2 + high(l1) * high(l2) + high(num);
+    s2 = low(num);
+    s3 = high(num);
+
+    PutUnsignedLongLong(&s3);
+    PutString(" ");
+    PutUnsignedLongLong(&s2);
+    PutString(" ");
+    PutUnsignedLongLong(&s1);
+    PutString(" ");
+    PutUnsignedLongLong(&s0);
+
+    res = s1 << 8 | s0;
+    carry = s3 << 8 | s2;
+
+    PutString("\r\n");
+    PutString("Result: ");
+    PutUnsignedLongLong(res);
+    PutUnsignedLongLong(carry);
+    PutString("\r\n");
 }
 
 int main(int argc, char *argv[])
 {
   unsigned char result[16];
   int i;
-  
+
   ClearScreen(0x07);
   SetCursorPosition(0, 0);
 
@@ -66,21 +105,21 @@ int main(int argc, char *argv[])
     PutString("Test : ");
     PutUnsignedLongLong(&cases[i].a);
     PutString(" * ");
-    PutUnsignedLongLong(&cases[i].b); 
+    PutUnsignedLongLong(&cases[i].b);
     PutString("\r\n");
-   
+
     PutString("    == ");
     PutUnsignedLongLong(&cases[i].rh);
     PutUnsignedLongLong(&cases[i].rl);
     PutString("\r\n");
-    
+
     llmultiply2(cases[i].a, cases[i].b, result);
-    
-    //PutString("Result ");
-    //PutUnsignedLongLong(&result[8]);
-    //PutUnsignedLongLong(&result[0]); 
-    //PutString("\r\n");
-    
+    /*
+    PutString("Result ");
+    PutUnsignedLongLong(&result[8]);
+    PutUnsignedLongLong(&result[0]);
+    PutString("\r\n");
+    */
     PutString("\r\n");
   }
 
