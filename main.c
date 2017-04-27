69a91d3c304f273a944aa6f311ff52f31c609471
Oier Saizar Ugarte <oiesa638@li2-9.ida.liu.se>
2017-04-27 11:11:41 +0200

    C function working

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <libepc.h>
+#include <limits.h>
 
 void llmultiply(unsigned long long int l1,
                 unsigned long long int l2,
@@ -48,48 +49,49 @@ void PutUnsignedLongLong(unsigned long long int* ulli)
 }
 
 unsigned long int low (unsigned long long int num) {
-	return (((1L << 32) -1) & num);
+	return (num & 0xFFFFFFFF);
 }
 
 unsigned long int high (unsigned long long int num) {
-	return (num >> 32);
+	return ((num >> 32) & 0xFFFFFFFF);
 }
 
 void llmultiply2(unsigned long long int l1, unsigned long long int l2, unsigned char *result) {
 
-    unsigned long int s0, s1, s2, s3;
-    unsigned long int num = low(l1) * low(l2);
-    unsigned long long res, carry;
+    unsigned int *res = (unsigned int *) result;
+    unsigned long long int num;
+    unsigned long long int l1low = low(l1),
+                           l2low=low(l2),
+                           l1high=high(l1),
+                           l2high=high(l2);
+    int i;
 
-    s0 = low(num);
+    for (i = 0; i < 4; i++){
+      res[i] = 0;
+    }
 
-    num = high(l1) * low(l2) + high(num);
-    s1 = low(num);
-    s2 = high(num);
+    num = l1low * l2low;
+    res[0] = low(num);
+    res[1] = high(num);
 
-    num = s1 + low(l1) * high(l2);
-    s1 = low(num);
+    num = l1low * l2high;
+    if (ULONG_MAX - res[1] < low(num)) res[2]++;
+    res[1] += low(num);
 
-    num = s2 + high(l1) * high(l2) + high(num);
-    s2 = low(num);
-    s3 = high(num);
+    if (ULONG_MAX - res[2] < high(num)) res[3]++;
+    res[2] += high(num);
 
-    PutUnsignedLongLong(&s3);
-    PutString(" ");
-    PutUnsignedLongLong(&s2);
-    PutString(" ");
-    PutUnsignedLongLong(&s1);
-    PutString(" ");
-    PutUnsignedLongLong(&s0);
+    num = l1high * l2low;
+    if (ULONG_MAX - res[1] < low(num)) res[2]++;
+    res[1] += low(num);
+    if (ULONG_MAX - res[2] < high(num)) res[3]++;
+    res[2] += high(num);
 
-    res = s1 << 8 | s0;
-    carry = s3 << 8 | s2;
+    num = l1high * l2high;
+    if (ULONG_MAX - res[2] < low(num)) res[3]++;
+    res[2] += low(num);
+    res[3] += high(num);
 
-    PutString("\r\n");
-    PutString("Result: ");
-    PutUnsignedLongLong(res);
-    PutUnsignedLongLong(carry);
-    PutString("\r\n");
 }
 
 int main(int argc, char *argv[])
@@ -100,7 +102,7 @@ int main(int argc, char *argv[])
   ClearScreen(0x07);
   SetCursorPosition(0, 0);
 
-  for (i = 0; i < 6; ++i)
+  /*for (i = 0; i < 6; ++i)
   {
     PutString("Test : ");
     PutUnsignedLongLong(&cases[i].a);
@@ -114,14 +116,32 @@ int main(int argc, char *argv[])
     PutString("\r\n");
 
     llmultiply2(cases[i].a, cases[i].b, result);
-    /*
+
     PutString("Result ");
     PutUnsignedLongLong(&result[8]);
     PutUnsignedLongLong(&result[0]);
     PutString("\r\n");
-    */
+
     PutString("\r\n");
+  }*/
+
+  // test assembly
+  PutString("Testing assembly for 1000000 iterations:");
+  PutString("\r\n");
+  for (i = 0; i < 1000000; i++){
+    llmultiply(cases[0].a+i, cases[0].b, result);
   }
+  PutString("Time: ");
+  PutString("\r\n");
+
+  // test c
+  PutString("Testing C for 1000000 iterations:");
+  PutString("\r\n");
+  for (i = 0; i < 1000000; i++){
+    llmultiply2(cases[0].a+i, cases[0].b, result);
+  }
+  PutString("Time: ");
+  PutString("\r\n");
 
   return 0;
 }
